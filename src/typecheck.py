b60113589684d72a35075c3ffc358528eb3aa57f
mathieu <mathieu.c.sabourin@gmail.com>
2018-03-31 16:23:59 -0400

    [pkg] Move code to src.

--- src/typecheck.py
+++ src/typecheck.py
@@ -73,30 +73,3 @@ def typecheck(obj: typing.Any) -> bool:
     }
     print(is_valid)
     return all(is_valid.values())
-
-
-class Mine:
-    i: int
-    c: str
-    d: typing.Dict[str, int]
-    l: typing.List[str]
-    t: typing.Tuple[int, str, int]
-    opt: typing.Optional[int]
-
-    def __init__(self, i, c, d, l, t, opt=None):
-        self.i = i
-        self.c = c
-        self.d = d
-        self.l = l
-        self.t = t
-        self.opt = opt
-
-
-if __name__ == '__main__':
-    good = Mine(42, 'lol', {'k': 42}, ['lol'], (1, 'l', 1))
-    bad = Mine('', 'lol', {'k': 42}, ['lol'], (1, 'l', 1))
-    bad_args = Mine('', 'lol', {'k': 42}, ['lol', 1], (1, 'l', 1))
-
-    assert typecheck(good), f'{good} should be valid.'
-    assert not typecheck(bad), f'{bad} should not be valid.'
-    assert not typecheck(bad_args), f'{bad_args} should not be valid.'
